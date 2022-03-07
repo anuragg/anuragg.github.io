711c7f2eb656a0e8e4b032ec272d25e05d1d763c
anurag <anurag.itbhu05@gmail.com>
2022-03-07 09:31:56 +0530

    t Revert "changes"
    This reverts commit d11b516a45eb4ab16f0acf3bb18229b91d3b4077.

--- _next/static/chunks/pages/components/nudge-page-abe938a7e837503f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[144],{5359:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/nudge-page",function(){return u(1674)}])}},function(n){n.O(0,[674,774,888,179],(function(){return _=5359,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
